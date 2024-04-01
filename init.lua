5d328a388a0842a61155077873fe932c7aeb5431
Patt <patrick.kings@live.com>
2024-04-01 14:38:43 +0300

    remove treesitter toggle on open

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
 local auto_cmd = vim.api.nvim_create_autocmd
 
 -- open nvim tree when vim starts
-auto_cmd({ "VimEnter" }, {
-  pattern = "*",
-  command = "NvimTreeToggle",
-})
+-- auto_cmd({ "VimEnter" }, {
+--   pattern = "*",
+--   command = "NvimTreeToggle",
+-- })
 
 -- format rust files
 auto_cmd({ "BufWritePre" }, {
